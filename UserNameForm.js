aa1cb409ef33ce9db11f7726bc0d96078283fc59
dragosconstalexandru <dragos.constantin93@gmail.com>
2022-10-21 11:21:16 +0300

    Taking value from input

---
+++ UserNameForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {octokit} from '../helpers/octokit';
+//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
+
+class UserNameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        (async () => {
+            const response = await octokit.request('GET /users/{username}/gists', {
+                username: this.state.value
+            });
+            if (response.status === 200 ) {
+                this.props.setGistsOnParent(response.data, response.data[0].owner);
+            }
+        })();
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+export default UserNameForm
